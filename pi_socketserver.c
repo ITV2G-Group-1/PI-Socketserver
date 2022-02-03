f3c17114903e558c0898963085ee1d63977209dc
luckyluuk28 <38768606+luckyluuk28@users.noreply.github.com>
2022-02-03 21:07:32 +0100

    Better error messages for json objects

--- pi_socketserver.c
+++ pi_socketserver.c
@@ -1,3 +1,10 @@
+/*
+TO ADD
+socket rewrite
+uuid exists check
+comments
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -8,14 +15,15 @@
 #include <json-c/json.h>
 #include <mysql/mysql.h>
 
+#define PORT 17021
+
 #define json_get_ex(datapoint, var, result) (json_object_object_get_ex(datapoint, var, result))
 #define json_get_id(array, i) (json_object_array_get_idx(array, i))
 #define json_length(array) (json_object_array_length(array))
 #define json_stringify(var) (json_object_get_string(var))
+#define json_object_stringify(obj) (json_object_to_json_string(obj))
 #define json_intify(var) (json_object_get_int(var))
 #define json_doublify(var) (json_object_get_double(var))
-
-#define PORT 17021
 
 int create_socket();
 int json_reader(char *json_string);
@@ -27,7 +35,8 @@ int insert_light_data(int id, int light_intensity, char time[32]);
 int insert_gps_data(int id, double gps_long, double gps_lat, char time[32]);
 int create_database();
 
-static const char time_format[] = "%Y-%m-%d %X";
+static const char datetime_format[] = "%Y-%m-%d %X";
+static const char time_format[] = "%X";
 static const char *server = "localhost";
 static const char *user = "main";
 static const char *passwd = "";
@@ -115,86 +124,94 @@ int json_reader(char *json_string) {
 
     time_t t;
 	struct tm lt;
-	char time[32];
+	char ESP_time[32];
+    char curr_time[32];
+	
+    t = time(NULL);
+	lt = *localtime(&t);
+	strftime(curr_time, sizeof(curr_time), time_format, &lt);
 
     parsed_json = json_tokener_parse(json_string);
+    if (!parsed_json) {
+		fprintf(stderr, "Message contain invalid json: \n%s\n\n", json_string);
+	}
 
     json_get_ex(parsed_json, "uuid", &uuid);
     if (!uuid) {
-        fprintf(stderr, "Error occured while trying to get \"uuid\" from: \n%s\n", json_string);
+        fprintf(stderr, "Json contains no or an invalid \"uuid\": \n%s\n\n", json_string);
         return 1;
     }
-
+    
     strcpy(c_uuid, json_stringify(uuid));
     id = get_uuid_id(c_uuid);
 
 	json_get_ex(parsed_json, "data", &data);
     if (!data) {
-        fprintf(stderr, "Error occured while trying to get \"data\" from: \n%s\n", json_string);
+        fprintf(stderr, "Json contains no or invalid \"data\": \n%s\n\n", json_string);
         return 1;
     }
-
+	
     n_data = json_length(data);
-    fprintf(stdout, "uuid: %s send %d datapoints\n", c_uuid, n_data);
+    fprintf(stdout, "[%s] uuid: %s send %d datapoints\n", curr_time ,c_uuid, n_data);
     for (i = 0; i < n_data; i++) {
         datapoint = json_get_id(data, i);
 
         json_get_ex(datapoint, "type", &type);
         if (!type) {
-            fprintf(stderr, "Error occured while trying to get \"type\" at datapoint %d from: \n%s\n", i, json_string);
+            fprintf(stderr, "Error occured while trying to get \"type\": \n%s\n", json_object_stringify(datapoint));
             break;
         }
 
         json_get_ex(datapoint, "timestamp", &timestamp);
         if (!timestamp) {
-            fprintf(stderr, "Error occured while trying to get \"timestamp\" at datapoint %d from: \n%s\n", i, json_string);
+            fprintf(stderr, "Error occured while trying to get \"timestamp\": \n%s\n", json_object_stringify(datapoint));
             break;
         }
 
         t = json_doublify(timestamp);
 		lt = *localtime(&t);
-		strftime(time, sizeof(time), time_format, &lt);
+		strftime(ESP_time, sizeof(ESP_time), datetime_format, &lt);
 
         strcpy(c_type, json_stringify(type));
         if (strcmp(c_type, "temperature") == 0) {
             json_get_ex(datapoint, "value", &value);
             d_temp = json_doublify(value);
             if (!d_temp) {
-                fprintf(stderr, "Error occured while trying to get \"temperature\" at datapoint %d from: \n%s\n", i, json_string);
-                break;
+                fprintf(stderr, "Error occured while trying to get \"temperature\": \n%s\n", json_object_stringify(datapoint));
+                continue;
             }
 
-            insert_temp_data(id, d_temp, time);
+            insert_temp_data(id, d_temp, ESP_time);
 
         } else if (strcmp(c_type, "light") == 0) {
             json_get_ex(datapoint, "value", &value);
             i_light = json_intify(value);
             if (!i_light) {
-                fprintf(stderr, "Error occured while trying to get \"light intensity\" at datapoint %d from: \n%s\n", i, json_string);
-                break;
+                fprintf(stderr, "Error occured while trying to get \"light intensity\": \n%s\n", json_object_stringify(datapoint));
+                continue;
             }
             
-            insert_light_data(id, i_light, time);
+            insert_light_data(id, i_light, ESP_time);
 
         } else if (strcmp(c_type, "gps") == 0) {
             json_get_ex(datapoint, "long", &gps_long);
             d_long = json_doublify(gps_long);
             if (!d_long) {
-                fprintf(stderr, "Error occured while trying to get \"longitude\" at datapoint %d from: \n%s\n", i, json_string);
-                break;
+                fprintf(stderr, "Error occured while trying to get \"longitude\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
+                continue;
             }
 
             json_get_ex(datapoint, "lat", &gps_lat);
             d_lat = json_doublify(gps_lat);
             if (!d_lat) {
-                fprintf(stderr, "Error occured while trying to get \"latitude\" at datapoint %d from: \n%s\n", i, json_string);
-                break;
+                fprintf(stderr, "Error occured while trying to get \"latitude\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
+                continue;
             }
 
-            insert_gps_data(id, d_long, d_lat, time);
+            insert_gps_data(id, d_long, d_lat, ESP_time);
 
         } else {
-            fprintf(stderr, "Error occured while trying to find the \"type\" at datapoint %d from: \n%s\n", i, json_string);
+            fprintf(stderr, "Error occured while trying to find the \"type\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
             break;
         }
     }
@@ -254,7 +271,7 @@ int insert_light_data(int id, int light_intensity, char time[32]) {
 }
 
 int insert_gps_data(int id, double gps_long, double gps_lat, char time[32]) {
-    sprintf(query, "INSERT INTO GPSData (GPS_ESP_id, GPS_DateTimeFromESP, GPS_long, GPS_lat) VALUES ('%d', '%s', '%.6f', '%.6f')", id, time, gps_long, gps_lat);
+    sprintf(query, "INSERT INTO GPSData (GPS_ESP_id, GPS_DateTimeFromESP, GPS_long, GPS_lat) VALUES ('%d', '%s', '%.9f', '%.8f')", id, time, gps_long, gps_lat);
     if (mysql_query(con, query)) return sql_err();
     return 0;
 }
@@ -289,7 +306,7 @@ int create_database() {
 			Light_ESP_id INT NOT NULL,\
 			Light_DateTimeFromESP DATETIME(2) NOT NULL,\
 			Light_TimestampAdded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
-			Light_Intensity SMALLINT(0) NOT NULL,\
+			Light_Intensity DECIMAL(11,4) NOT NULL,\
 			PRIMARY KEY (ESP_id, DateTimeFromESP),\
 			CONSTRAINT fk_LightIntensityData_ESPs\
 				FOREIGN KEY (ESP_id)\
