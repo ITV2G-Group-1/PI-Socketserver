cc5d7bfa0481483908b48882140222a3c17b9494
luckyluuk28 <38768606+luckyluuk28@users.noreply.github.com>
2022-02-05 20:00:44 +0100

    working build, no uuid check (had errors)

--- pi_socketserver.c
+++ pi_socketserver.c
@@ -8,8 +8,7 @@
 #include <json-c/json.h>
 #include <mysql/mysql.h>
 
-#define PORT 17021
-
+#define ensure_db_con() (con ? 0 : connect_to_database())
 #define json_get_ex(datapoint, var, result) (json_object_object_get_ex(datapoint, var, result))
 #define json_get_id(array, i) (json_object_array_get_idx(array, i))
 #define json_length(array) (json_object_array_length(array))
@@ -17,6 +16,9 @@
 #define json_object_stringify(obj) (json_object_to_json_string(obj))
 #define json_intify(var) (json_object_get_int(var))
 #define json_doublify(var) (json_object_get_double(var))
+
+#define PORT 17021
+#define BUFFERSIZE 2048
 
 static int create_socket();
 static int json_reader(char *json_string);
@@ -35,7 +37,8 @@ static const char *user = "main";
 static const char *passwd = "";
 static const char *database = "testdb";
 
-static char query[256];
+static char query[512];
+static char *buffer;
 
 static MYSQL *con;
 static MYSQL_RES *res;
@@ -46,55 +49,63 @@ static struct sockaddr_in address;
 static int addrlen = sizeof(address);
 
 int main(int argc, char const *argv[]) {
-    connect_to_database();
-    create_socket();
-       
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0) {
-        fprintf(stderr, "bind failed");
-        return 1;
-    }
-
-    if (listen(server_fd, 3) < 0) {
-        fprintf(stderr, "listen");
+    connect_to_database(); // Automatically exits if error occurs
+    if (create_socket() != 0) return 1;
+    
+    buffer = malloc(BUFFERSIZE);
+    
+    // Listen for client connection
+    if (listen(server_fd, 5) < 0) {
+        fprintf(stderr, "[SOCK ERROR] Failure occured while listening for client connection");
         return 1;
     }
     
     while (1) {
-        char buffer[2048] = {0};
+        memset(buffer, 0, BUFFERSIZE);
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
                         (socklen_t*)&addrlen))<0) {
-            fprintf(stderr, "accept");
+            fprintf(stderr, "[SOCK ERROR] Failure occured while accepting a clients connection");
             return 1;
         }
     
-        read(new_socket, buffer, 2048);
+        read(new_socket, buffer, BUFFERSIZE);
         json_reader(buffer);
+        memset(buffer, 0, BUFFERSIZE);
     }
     return 0;
 }
 
-int create_socket() {
+static int create_socket() {
     int opt = 1;
 
+    // Create socket, AF_INET = net, AF_LOCAL = local
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        fprintf(stderr, "socket failed");
+        fprintf(stderr, "[SOCK ERROR] Creation of socket failed\n");
         return 1;
     }
        
+    // Set socket options (prevents "already in use" errors)
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt))) {
-        fprintf(stderr, "setsockopt");
+        fprintf(stderr, "[SOCK ERROR] Setting socket options failed\n");
         return 1;
     }
     
+    // Set values to address struct
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-	return 0;
+    address.sin_port = htons(PORT);
+
+    // Binds socket to address and port   
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0) {
+        fprintf(stderr, "[SOCK ERROR] Binding of socket failed\n");
+        return 1;
+    }
+    return 0;
 }
 
-int json_reader(char *json_string) {
+static int json_reader(char *json_string) {
     struct json_object *parsed_json;
 	struct json_object *uuid;
 	struct json_object *data;
@@ -127,26 +138,26 @@ int json_reader(char *json_string) {
 
     parsed_json = json_tokener_parse(json_string);
     if (!parsed_json) {
-		fprintf(stderr, "Message contain invalid json: \n%s\n\n", json_string);
+		fprintf(stderr, "[JSON ERROR] Message contained invalid json: \n%s\n\n", json_string);
         return 1;
 	}
 
     json_get_ex(parsed_json, "uuid", &uuid);
     if (!uuid) {
-        fprintf(stderr, "Json contains no or an invalid \"uuid\": \n%s\n\n", json_string);
+        fprintf(stderr, "[JSON ERROR] Couldn't acquire \"uuid\" from: \n%s\n\n", json_string);
         return 1;
     }
-    
+
     strcpy(c_uuid, json_stringify(uuid));
     id = get_uuid_id(c_uuid);
     if (!id) {
-        fprintf(stderr, "Err occured trying to get id from uuid: %s\n", c_uuid);
+        fprintf(stderr, "[SQL ERROR] Couldn't acquire id from uuid: %s\n", c_uuid);
         return 1;
     }
 
 	json_get_ex(parsed_json, "data", &data);
     if (!data) {
-        fprintf(stderr, "Json contains no or invalid \"data\": \n%s\n\n", json_string);
+        fprintf(stderr, "[JSON ERROR] Couldn't acquire \"data\": \n%s\n\n", json_string);
         return 1;
     }
 	
@@ -157,13 +168,13 @@ int json_reader(char *json_string) {
 
         json_get_ex(datapoint, "type", &type);
         if (!type) {
-            fprintf(stderr, "Error occured while trying to get \"type\": \n%s\n", json_object_stringify(datapoint));
+            fprintf(stderr, "[JSON ERROR] Couldn't acquire \"type\" from: \n%s\n", json_object_stringify(datapoint));
             continue;
         }
 
         json_get_ex(datapoint, "timestamp", &timestamp);
         if (!timestamp) {
-            fprintf(stderr, "Error occured while trying to get \"timestamp\": \n%s\n", json_object_stringify(datapoint));
+            fprintf(stderr, "[JSON ERROR] Couldn't acquire \"timestamp\" from: \n%s\n", json_object_stringify(datapoint));
             continue;
         }
 
@@ -175,8 +186,8 @@ int json_reader(char *json_string) {
         if (strcmp(c_type, "temperature") == 0) {
             json_get_ex(datapoint, "value", &value);
             d_temp = json_doublify(value);
-            if (!d_temp) {
-                fprintf(stderr, "Error occured while trying to get \"temperature\": \n%s\n", json_object_stringify(datapoint));
+            if (!d_temp || d_temp >= 100) {
+                fprintf(stderr, "[JSON ERROR] Couldn't acquire \"temperature\" from: \n%s\n", json_object_stringify(datapoint));
                 continue;
             }
 
@@ -186,44 +197,44 @@ int json_reader(char *json_string) {
             json_get_ex(datapoint, "value", &value);
             i_light = json_intify(value);
             if (!i_light) {
-                fprintf(stderr, "Error occured while trying to get \"light intensity\": \n%s\n", json_object_stringify(datapoint));
+                fprintf(stderr, "[JSON ERROR] Couldn't acquire \"light intensity\" from: \n%s\n", json_object_stringify(datapoint));
                 continue;
             }
             
             insert_light_data(id, i_light, ESP_time);
 
         } else if (strcmp(c_type, "gps") == 0) {
-            json_get_ex(datapoint, "long", &gps_long);
+            json_get_ex(datapoint, "lng", &gps_long);
             d_long = json_doublify(gps_long);
             if (!d_long) {
-                fprintf(stderr, "Error occured while trying to get \"longitude\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
+                fprintf(stderr, "[JSON ERROR] Couldn't acquire \"longitude\" from: \n%s\n", json_object_stringify(datapoint));
                 continue;
             }
 
             json_get_ex(datapoint, "lat", &gps_lat);
             d_lat = json_doublify(gps_lat);
             if (!d_lat) {
-                fprintf(stderr, "Error occured while trying to get \"latitude\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
+                fprintf(stderr, "[JSON ERROR] Couldn't acquire \"latitude\" from: \n%s\n", json_object_stringify(datapoint));
                 continue;
             }
 
             insert_gps_data(id, d_long, d_lat, ESP_time);
 
         } else {
-            fprintf(stderr, "Error occured while trying to find the \"type\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
+            fprintf(stderr, "[JSON ERROR] Couldn't acquire \"type\" at datapoint %d from: \n%s\n\n", i, json_object_stringify(datapoint));
             continue;
         }
     }
     return 0;
 }
 
-int sql_err() {
-	fprintf(stderr, "%s\n", mysql_error(con));
+static int sql_err() {
+	fprintf(stderr, "[SQL ERROR] %s\n", mysql_error(con));
 	if (con) mysql_close(con);
-	return(1);
+	exit(1);
 }
 
-int connect_to_database() {
+static int connect_to_database() {
     con = mysql_init(NULL);
     if (!con) sql_err();
 
@@ -232,37 +243,27 @@ int connect_to_database() {
 
     sprintf(query, "USE %s", database);
     if (mysql_query(con, query)) {
-        fprintf(stderr, "%s\ncreating database '%s'\n", mysql_error(con), database);
-        if (!create_database() == 0) return 1;
+        fprintf(stderr, "[SQL ERROR] %s\nCreating database '%s'\n", mysql_error(con), database);
+        create_database(); // Automatically exits if error occurs
     }
 	return 0;
 }
 
-int get_uuid_id(char uuid[20]) {
+static int get_uuid_id(char uuid[20]) {
 	int id;
 
-	sprintf(query, "SELECT IFNULL(SELECT id FROM ESPs WHERE uuid='%s', NULL)", uuid);
+	sprintf(query, "SELECT id FROM ESPs WHERE uuid='%s'", uuid);
 	mysql_query(con, query);
 
 	res = mysql_store_result(con);
 	
 	if (!res) {
-        fprintf(stderr, "uuid: %s not found, inserting it into ESPs table\n", uuid);
-        mysql_free_result(res);
-
-        sprintf(query, "INSERT INTO ESPs (uuid) VALUES ('%s')", uuid);
-        mysql_query(con, query);
-
-        mysql_query(con, "SELECT LAST_INSERT_ID()");
-        res = mysql_store_result(con);
-        if (!res) {
-            sql_err();
-            return 0;
-        }
+        fprintf(stderr, "[SQL ERROR] %s\n", mysql_error(con));
+        return 0;
     }
     
     while((row = mysql_fetch_row(res)) !=0) {
-		id = row[0] ? atof(row[0]) : 0;
+		id = row[0] ? atof(row[0]) : 0.0f;
 
 		mysql_free_result(res);
 		return id;
@@ -270,76 +271,86 @@ int get_uuid_id(char uuid[20]) {
     return 0;
 }
 
-int insert_temp_data(int id, double temp, char time[32]) {
+static int insert_temp_data(int id, double temp, char time[32]) {
+    ensure_db_con(); // Automatically exists if error occurs
+
     sprintf(query, "INSERT INTO TempData (Temp_ESP_id, Temp_DateTimeFromESP, Temp_Temp) VALUES (%d, '%s', %.8f)", id, time, temp);
-    if (mysql_query(con, query)) return sql_err();
+    if (mysql_query(con, query)) sql_err();
     return 0;
 }
 
-int insert_light_data(int id, int light_intensity, char time[32]) {
+static int insert_light_data(int id, int light_intensity, char time[32]) {
+    ensure_db_con(); // Automatically exists if error occurs
+
     sprintf(query, "INSERT INTO LightIntensityData (Light_ESP_id, Light_DateTimeFromESP, Light_Intensity) VALUES ('%d', '%s', '%d')", id, time, light_intensity);
-    if (mysql_query(con, query)) return sql_err();
+    if (mysql_query(con, query)) sql_err();
     return 0;
 }
 
-int insert_gps_data(int id, double gps_long, double gps_lat, char time[32]) {
+static int insert_gps_data(int id, double gps_long, double gps_lat, char time[32]) {
+    ensure_db_con(); // Automatically exists if error occurs
+
     sprintf(query, "INSERT INTO GPSData (GPS_ESP_id, GPS_DateTimeFromESP, GPS_long, GPS_lat) VALUES ('%d', '%s', '%.9f', '%.8f')", id, time, gps_long, gps_lat);
-    if (mysql_query(con, query)) return sql_err();
+    if (mysql_query(con, query)) sql_err();
     return 0;
 }
 
-int create_database() {
-    if (!con) return sql_err();
+static int create_database() {
+    ensure_db_con(); // Automatically exists if error occurs
 
     sprintf(query, "CREATE DATABASE IF NOT EXISTS %s", database);
-	if (mysql_query(con, query)) return sql_err();
+	if (mysql_query(con, query)) sql_err();
 
-	if (mysql_query(con, "CREATE TABLE IF NOT EXISTS ESPs(\
+    sprintf(query, "CREATE TABLE IF NOT EXISTS ESPs(\
 			id INT NOT NULL AUTO_INCREMENT,\
 			uuid CHAR(20) NOT NULL,\
 			PRIMARY KEY (id),\
 			UNIQUE INDEX id_UNIQUE (id ASC))\
-			ENGINE = InnoDB;")) return sql_err();		
+			ENGINE = InnoDB;");
+	if (mysql_query(con, query)) sql_err();		
 	
-    if (mysql_query(con, "CREATE TABLE IF NOT EXISTS TempData(\
+    sprintf(query, "CREATE TABLE IF NOT EXISTS TempData(\
 			Temp_ESP_id INT NOT NULL,\
 			Temp_DateTimeFromESP DATETIME(2) NOT NULL,\
 			Temp_TimestampAdded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
 			Temp_Temp DECIMAL(3,1) NOT NULL,\
-			PRIMARY KEY (ESP_id, DateTimeFromESP),\
+			PRIMARY KEY (Temp_ESP_id, DateTimeFromESP),\
 			CONSTRAINT fk_TempData_ESPs\
-				FOREIGN KEY (ESP_id)\
-				REFERENCES testdb.ESPs (id)\
+				FOREIGN KEY (Temp_ESP_id)\
+				REFERENCES %s.ESPs (id)\
 				ON DELETE NO ACTION\
 				ON UPDATE NO ACTION)\
-			ENGINE = InnoDB;")) return sql_err();
+			ENGINE = InnoDB;", database);
+    if (mysql_query(con, query)) sql_err();
 	
-	if (mysql_query(con, "CREATE TABLE IF NOT EXISTS LightIntensityData(\
+    sprintf(query, "CREATE TABLE IF NOT EXISTS LightIntensityData(\
 			Light_ESP_id INT NOT NULL,\
 			Light_DateTimeFromESP DATETIME(2) NOT NULL,\
 			Light_TimestampAdded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
 			Light_Intensity SMALLINT NOT NULL,\
-			PRIMARY KEY (ESP_id, DateTimeFromESP),\
+			PRIMARY KEY (Light_ESP_id, DateTimeFromESP),\
 			CONSTRAINT fk_LightIntensityData_ESPs\
-				FOREIGN KEY (ESP_id)\
-				REFERENCES testdb.ESPs (id)\
+				FOREIGN KEY (Light_ESP_id)\
+				REFERENCES %s.ESPs (id)\
 				ON DELETE NO ACTION\
 				ON UPDATE NO ACTION)\
-			ENGINE = InnoDB;")) return sql_err();
+			ENGINE = InnoDB;", database);
+	if (mysql_query(con, query)) sql_err();
 
-	if (mysql_query(con, "CREATE TABLE IF NOT EXISTS GPSData(\
+    sprintf(query, "CREATE TABLE IF NOT EXISTS GPSData(\
 			GPS_ESP_id INT NOT NULL,\
 			GPS_DateTimeFromESP DATETIME(2) NOT NULL,\
 			GPS_TimestampAdded TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\
 			GPS_long DECIMAL(9,6) NOT NULL,\
 			GPS_lat DECIMAL(8,6) NOT NULL,\
-			PRIMARY KEY (ESP_id, DateTimeFromESP),\
+			PRIMARY KEY (GPS_ESP_id, GPS_DateTimeFromESP),\
 			CONSTRAINT fk_GPSData_ESPs\
-				FOREIGN KEY (ESP_id)\
-				REFERENCES testdb.ESPs (id)\
+				FOREIGN KEY (GPS_ESP_id)\
+				REFERENCES %s.ESPs (id)\
 				ON DELETE NO ACTION\
 				ON UPDATE NO ACTION)\
-			ENGINE = InnoDB;")) return sql_err();
+			ENGINE = InnoDB;", database);
+	if (mysql_query(con,  query)) sql_err();
 
 	return(0);
 }
